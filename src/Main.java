345a5595186808086405b8be16f63e4f1fe60df8
Lidak <you@example.com>
2024-10-10 16:53:54 +0200

    zmiana branch

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ public class Main {
             System.out.println("i = " + i);
             System.out.println("Siema");
             System.out.println("yo yo yo");
+            System.out.println("branch pollub 1");
         }
     }
 }
