d07a8a2aa97f4fc76f03a21daae50e7228d35ad5
Lidak <you@example.com>
2024-10-10 16:50:37 +0200

    zmiana 3

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
             System.out.println("Siema");
-            System.out.println("Siema2");
+            System.out.println("yo yo yo");
         }
     }
 }
